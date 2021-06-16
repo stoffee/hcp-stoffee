63264f151bf71f7df88f1706f62e6d10a2d437db
stoffee <chris@roadkill.org>
2021-06-16 11:57:07 -0700

    updates

--- main.tf
+++ main.tf
@@ -14,10 +14,12 @@ provider "hcp" {}
 // Use the cloud provider AWS to provision resources that will be connected to HCP
 provider "aws" {
   region = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 // Create an HVN
-resource "hcp_hvn" "example_hvn" {
+resource "hcp_hvn" "my_hvn" {
   hvn_id         = "hcp-hvn-${var.prefix}"
   cloud_provider = "aws"
   region         = var.region
@@ -39,31 +41,31 @@ resource "aws_vpc_peering_connection_accepter" "main" {
 }
 
 // Create a network peering between the HVN and the AWS VPC
-resource "hcp_aws_network_peering" "example" {
-  hvn_id          = hcp_hvn.example_hvn.hvn_id
-  peering_id      = "hcp-tf-example-peering"
+resource "hcp_aws_network_peering" "hvn_peering" {
+  hvn_id          = hcp_hvn.my_hvn.hvn_id
+  peering_id      = "hcp-${var.prefix}-peering"
   peer_vpc_id     = aws_vpc.main.id
   peer_account_id = aws_vpc.main.owner_id
   peer_vpc_region = data.aws_arn.main.region
 }
 
 // Create an HVN route that targets your HCP network peering and matches your AWS VPC's CIDR block
-resource "hcp_hvn_route" "example" {
+resource "hcp_hvn_route" "hvn_route" {
   hvn_link         = hcp_hvn.hvn.self_link
-  hvn_route_id     = "hcp-tf-example-hvn-route"
+  hvn_route_id     = "hcp-${var.stoffee}-hvn-route"
   destination_cidr = aws_vpc.main.cidr_block
   target_link      = hcp_aws_network_peering.example.self_link
 }
 
 // Create a Consul cluster in the same region and cloud provider as the HVN
-resource "hcp_consul_cluster" "example" {
-  hvn_id     = hcp_hvn.example_hvn.hvn_id
-  cluster_id = "hcp-tf-example-consul-cluster"
+resource "hcp_consul_cluster" "hcp_consul" {
+  hvn_id     = hcp_hvn.my_hvn.hvn_id
+  cluster_id = "hcp-${var.prefix}-consul-cluster"
   tier       = "development"
 }
 
 // Create a Vault cluster in the same region and cloud provider as the HVN
-resource "hcp_vault_cluster" "example" {
-  cluster_id = "hcp-tf-example-vault-cluster"
-  hvn_id     = hcp_hvn.example_hvn.hvn_id
+resource "hcp_vault_cluster" "hcp_vault" {
+  cluster_id = "hcp-${var.prefix}-vault-cluster"
+  hvn_id     = hcp_hvn.my_hvn.hvn_id
 }
--- vars.tf
+++ vars.tf
@@ -1,5 +1,14 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
 variable "prefix" {
   type        = string
   description = "Prefix used in resource names"
   default     = "hashicorp"
+}
+
+variable "region" {
+  type        = string
+  description = "The Azure region to deploy resources to"
+  default = "us-west-2"
 }
